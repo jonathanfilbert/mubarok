48c9cecf5908f755738392873ce9bac8a58b7001
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-05-16 18:37:36 +0700

    fix: disable staticfiles

--- mubarok/settings.py
+++ mubarok/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'backend'
 ]
 
@@ -119,16 +118,4 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
-
-CSRF_COOKIE_SECURE = False
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# https://docs.djangoproject.com/en/3.0/howto/static-files
