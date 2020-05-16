8de5827732b8c88218327206153e9cbf42187049
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-05-16 18:33:25 +0700

    fix: static url

--- mubarok/settings.py
+++ mubarok/settings.py
@@ -123,5 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
 CSRF_COOKIE_SECURE = False
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
