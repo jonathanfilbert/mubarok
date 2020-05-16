21ffaf98dbd69859c5c379e870b9f4bde0607396
Jonathanfilbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-05-16 18:41:29 +0700

    fix: disable staticfiles

--- mubarok/settings.py
+++ mubarok/settings.py
@@ -14,6 +14,7 @@ import os
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 PRODUCTION = os.environ.get('DATABASE_URL') != None
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'backend'
 ]
 
@@ -118,4 +120,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+CSRF_COOKIE_SECURE = False
+
+STATICFILES_DIRS = [
+    os.path.join(os.path.normpath(BASE_DIR), "static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
